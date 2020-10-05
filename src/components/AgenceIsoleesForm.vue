3bf2b47d9737c8b9a882ac6f1fbd60c9943cd8f2
Emeline <A487423@FR07199639L.dsk.eur.msd.world.socgen>
2020-10-05 15:05:15 +0200

    verification if ref exist

--- src/components/AgenceIsoleesForm.vue
+++ src/components/AgenceIsoleesForm.vue
@@ -5,12 +5,9 @@
 			<div slot="header">
 				<h4 class="card-header">Agences isolées</h4>
 			</div>
-			<input type="file" id="input" @change="importer($event)" style="margin-left:10px;"/>
+			<input type="file" id="input" @change="importer($event)" style="margin-left:10px;" />
 		</el-card>
 		<!-- Fin agences isolées-->
-
-
-
 	</div>
 </template>
 
@@ -24,15 +21,14 @@ import { constants } from 'crypto';
 import { log } from 'util';
 import { arraySlugToHeader, arraySlugifier, arrayToJSON } from '../etlUtils';
 
-
 export default {
 	data() {
 		return {
 			// Données énumérées venant de l'API
-            remoteEnum: {
-                priorites: [],
-                statut: [],
-                enseignes: [],
+			remoteEnum: {
+				priorites: [],
+				statut: [],
+				enseignes: [],
 				application_impactee: [],
 			},
 
@@ -56,49 +52,77 @@ export default {
 	},
 
 	methods: {
-
-
 		//////Partie Agence/////////
 		importer(event) {
-			
 			const file = event.target.files[0];
 			//console.log(file);
 
 			readXlsxFile(file).then(rows => {
+				Axios.get('http://localhost:5000/api/reference').then(
+					response => {
+						// On parcourt toutes les lignes du fichier Excel des agences
+						for (const row of rows) {
+							//console.log(row);
+							//console.log(row[0]);
+                            //console.log(row[7]);
+                            //console.log(row[2]);
 
-				/*for(let row of rows){
+                            /*if(row[7].includes('En cours')){
+                                console.log("j'accède au row 7");
+                            }else{
+                                console.log("je n'y accède pas ");
+                            }*/
 
-					for(let i = 0; i< row.length; i++){
-						console.log(row[i]);
+
+                            
+
+							// On vérifie que les différentes références du fichier Excel des agences sont présentes dans la main courante
+							/*if (
+									response.data[p].references.includes(
+										row[0]
+									) == true
+								) {
+									for (let row of rows) {
+										// Si l'état de l'incident est "En cours"
+										if (
+											row[7].includes('En cours') == true
+										) {
+											this.incident_id =
+											response.data[p].incident_id;
+										this.curID =
+											response.data[p].incident_id;
+
+											console.log(row[0] + ' En cours');
+											console.log(this.incident_id);
+										}
+										// Sinon si l'état de l'incident est "Clos"
+										else {
+											this.incident_id =
+											response.data[p].incident_id;
+										this.curID =
+											response.data[p].incident_id;
+										window.open(
+											'/#/maj-agence/id=' +
+												this.incident_id
+										);
+											console.log(row[0] + ' Clos');
+										}
+									}
+								} */
+						}
 					}
-				}*/
-
-
-				var agencesJSON = arrayToJSON(rows);
+				);
+				//var agencesJSON = arrayToJSON(rows);
 
 				//console.log(agencesJSON);
 				//console.log(this.remoteEnum);
 
-				for( const agence of agencesJSON){
+				/*for( const agence of agencesJSON){
 
 					console.log(agence.ref);
-				}
-
-				/*for (const agence of agencesJSON) {
-					agence.debut.setHours(agence.debut.getHours() - 14);
-					agence.fin_incident == null ? undefined : agence.fin_incident.setHours(agence.fin_incident.getHours() - 14);
-					// Statut
-					agence.etat.includes('Clos') ? agence.etat=5 : agence.etat=2
-					// Priorité
-
-					console.log(agence.prio);
-					//agence.prio = this.remoteEnum.priorites.find(el => el.priorite == agence.prio).id
-					console.log(agence.etat);
 				}*/
-
-			})
-
-		}
+			});
+		},
 	},
 };
 </script>
