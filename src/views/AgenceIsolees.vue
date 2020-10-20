2e67a8b2646849608b54535c0fabb79cd7183e74
[Emeline] <A487423@FR07199639L.dsk.eur.msd.world.socgen>
2020-10-20 16:50:03 +0200

    modif de la page agence.vue

--- src/views/AgenceIsolees.vue
+++ src/views/AgenceIsolees.vue
@@ -1,55 +1,18 @@
 <template>
 	<div style="100vh">
 		<base-header title="Agence Isolées"></base-header>
-		
 
-			<!-- <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
-				<el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
-			</el-table>-->
-
-
-			<agence-isolees-form></agence-isolees-form>
-		
+		<agence-isolees-form></agence-isolees-form>
 	</div>
 </template>
 
 <script>
 import AgenceIsoleesForm from '@/components/AgenceIsoleesForm';
-import Grid from '@/components/Grid.vue';
-import Splitpanes from 'splitpanes';
-import 'splitpanes/dist/splitpanes.css';
-import Axios from 'axios';
-import JsonExcel from 'vue-json-excel';
 
 export default {
 	components: {
-		Grid,
-		Splitpanes,
 		AgenceIsoleesForm,
-	},
-
-
-	data() {
-		return {
-			// tableData: [],
-			// tableHeader: [],
-		};
-	},
-
-	methods: {
-		// handleSuccess({ results, header }) {
-		// 	this.tableData = results;
-		// 	this.tableHeader = header;
-		// },
 	},
 };
 </script>
 
-<style lang="sass">
-
-div.splitpanes
-  height: calc(100vh - 71px)
-
-
-
-</style>
