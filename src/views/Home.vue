59765e289b3482cce2dd196fc32e1397fb826a5e
A487365 <A487365@FR07201170L.dsk.eur.msd.world.socgen>
2021-03-23 15:03:35 +0100

    Fix home page style with tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="home">
+    <div >
         <base-header title="Accueil" />
+        <div class="flex mt-2 flex-col items-center">
         <img alt="Vue logo" src="../assets/Logo AGIPRO et subtitle_v2.svg" />
-        <h1 class="welcomeHeader">Bienvenue dans AGIPROS</h1>
-        <p>
+        <h1 class="mt-4 font-semibold text-xl">Bienvenue dans AGIPROS</h1>
+        <p class="mt-2 text-lg">
             AGIPROS pour ATP, Gestion des Incidents, des PROblèmes et Securiprod,
             <br />
             est une plateforme destinée à simplifier et fiabiliser les
@@ -13,11 +14,11 @@
         <br />
         <br />
         <br />
-        <h1>Notre outil de supervision</h1>
+        <h1 class=" font-semibold text-xl">Notre outil de supervision :</h1>
         <a href="https://moncockpit.safe.socgen/#/" onclick="window.open(this.href); return false;"
             ><img alt="Vue logo" src="../assets/cockpit-icon-cropped.png"
         /></a>
-
+        </div>
         <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     </div>
 </template>
@@ -34,12 +35,6 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-img
-	margin-top: 30px
-div.home
-	p
-		font-size: 1.5em
-	h1.welcomeHeader
-		font-size: 3em
+<style >
+
 </style>
